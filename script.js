03c6d06e11e820850369dff1e233dc8b09ed1c12
towhidJ <towhidulislamcse@gmail.com>
2021-12-25 23:15:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var a=prompt("Enter UserName");
+//  var b=prompt("Enter password");
+//   	if(a== "Towhidul islam" && b=="27-06-1998")
+//   	{
+//   	    window.location.href="start.html";
+//   	}
+//   	else
+//   	{
+//   		alert("invalid details");
+//   		window.location.href="index.html";
+//   	}
+
+window.location.href = "start.html";
